6fbfa1dd5d6a0d875596bf58577f2dfe2917c5c6
Hao Xu <xu.h2@student.unimelb.edu.au>
2021-04-18 09:22:23 +1000

    added NFTminilist display

---
+++ components/nft/NFTlist.js
@@ -0,0 +1,81 @@
+import React from 'react';
+import NFTcardmini from './NFTcardmini';
+import { Grid } from '@material-ui/core';
+import { useRouter } from 'next/router';
+import useSWR from 'swr';
+
+const fetcher = (url) => fetch(url).then(console.log(url), (res) => res.json());
+
+function Items() {
+  const router = useRouter();
+  const { slug } = router.query;
+  const { data, error } = useSWR(
+    router.query.id ? `/api/user/${router.query.id}` : null,
+    fetcher
+  );
+
+  if (error) return <div>Failed to load user</div>;
+  if (!data) return <div>Loading...</div>;
+  console.log(data);
+
+  // var data = [
+  //   {
+  //     issuer: 'Bing Xu',
+  //     recipient: 'Hao Xu',
+  //     event: 'Random Event #1',
+  //     image:
+  //       'https://images.unsplash.com/photo-1554188248-986adbb73be4?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80',
+  //     description:
+  //       'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce ac ipsum purus. Donec varius vel turpis eget bibendum. Aenean commodo ipsum ligula, ut mollis neque suscipit nec. Integer sem enim, luctus fermentum sapien vel, cursus malesuada turpis. Donec at urna mauris. Nullam nec rhoncus ipsum, sed dictum quam. Morbi ut mi dolor. Integer lobortis tortor id sollicitudin ullamcorper.',
+  //   },
+  //   {
+  //     issuer: 'Bing Xu',
+  //     recipient: 'Hao Xu',
+  //     event: 'Random Event #2',
+  //     image:
+  //       'https://images.unsplash.com/photo-1581352102070-b7d62164e147?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1050&q=80',
+  //     description:
+  //       'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce ac ipsum purus. Donec varius vel turpis eget bibendum. Aenean commodo ipsum ligula, ut mollis neque suscipit nec. Integer sem enim, luctus fermentum sapien vel, cursus malesuada turpis. Donec at urna mauris. Nullam nec rhoncus ipsum, sed dictum quam. Morbi ut mi dolor. Integer lobortis tortor id sollicitudin ullamcorper.',
+  //   },
+  //   {
+  //     issuer: 'CISSA',
+  //     recipient: 'Hao Xu',
+  //     event: '2021 CodeBrew - Remote Education & Work 1st',
+  //     image: 'https://codebrew.com.au/images/remote_work.png',
+  //     description:
+  //       'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce ac ipsum purus. Donec varius vel turpis eget bibendum. Aenean commodo ipsum ligula, ut mollis neque suscipit nec. Integer sem enim, luctus fermentum sapien vel, cursus malesuada turpis. Donec at urna mauris. Nullam nec rhoncus ipsum, sed dictum quam. Morbi ut mi dolor. Integer lobortis tortor id sollicitudin ullamcorper.',
+  //   },
+  //   {
+  //     issuer: 'CISSA',
+  //     recipient: 'Hao Xu',
+  //     event: '2021 CodeBrew - Social Good 1st',
+  //     image: 'https://codebrew.com.au/images/social_good.png',
+  //     description:
+  //       'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce ac ipsum purus. Donec varius vel turpis eget bibendum. Aenean commodo ipsum ligula, ut mollis neque suscipit nec. Integer sem enim, luctus fermentum sapien vel, cursus malesuada turpis. Donec at urna mauris. Nullam nec rhoncus ipsum, sed dictum quam. Morbi ut mi dolor. Integer lobortis tortor id sollicitudin ullamcorper.',
+  //   },
+  //   {
+  //     issuer: 'CISSA',
+  //     recipient: 'Hao Xu',
+  //     event: '2021 CodeBrew - Public Health',
+  //     image: 'https://codebrew.com.au/images/mental_health.png',
+  //     description:
+  //       'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce ac ipsum purus. Donec varius vel turpis eget bibendum. Aenean commodo ipsum ligula, ut mollis neque suscipit nec. Integer sem enim, luctus fermentum sapien vel, cursus malesuada turpis. Donec at urna mauris. Nullam nec rhoncus ipsum, sed dictum quam. Morbi ut mi dolor. Integer lobortis tortor id sollicitudin ullamcorper.',
+  //   },
+  // ];
+
+  const getNFTMini = (NFTObj) => {
+    return (
+      <Grid item xs={6} sm={3}>
+        <NFTcardmini {...NFTObj} />
+      </Grid>
+    );
+  };
+
+  return (
+    <Grid container spacing={1} justify='center'>
+      {data.map((NFTObj) => getNFTMini(NFTObj))}
+    </Grid>
+  );
+}
+
+export default Items;
--- pages/profile/[slug].js
+++ pages/profile/[slug].js
@@ -1,8 +1,8 @@
-import React from "react";
-import {makeStyles} from "@material-ui/core/styles";
-import Grid from "@material-ui/core/Grid";
-import Card from "@material-ui/core/Card";
-import Typography from "@material-ui/core/Typography";
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Grid from '@material-ui/core/Grid';
+import Card from '@material-ui/core/Card';
+import Typography from '@material-ui/core/Typography';
 import {
   CardActionArea,
   CardContent,
@@ -10,108 +10,112 @@ import {
   Box,
   Slide,
   IconButton,
-} from "@material-ui/core";
-import FileCopyOutlinedIcon from "@material-ui/icons/FileCopyOutlined";
-import NFTcardmini from "../../components/nft/NFTcardmini";
-import {useRouter} from "next/router";
+} from '@material-ui/core';
+import FileCopyOutlinedIcon from '@material-ui/icons/FileCopyOutlined';
+import NFTcardmini from '../../components/nft/NFTcardmini';
+import NFTlist from '../../components/nft/NFTlist';
+
+import { useRouter } from 'next/router';
 import useSwr from 'swr';
 
 const useStyles = makeStyles({
   root: {
     borderRadius: 0,
-    position: "relative",
-    width: "100%",
-    height: "20rem",
+    position: 'relative',
+    width: '100%',
+    height: '20rem',
     zIndex: 1,
-    display: "flex",
-    flexWrap: "wrap",
-    justifyContent: "left",
-    paddingLeft: "32px",
-    alignContent: "center",
-    overflow: "hidden",
-    boxShadow: "none",
-    background: "none",
+    display: 'flex',
+    flexWrap: 'wrap',
+    justifyContent: 'left',
+    paddingLeft: '32px',
+    alignContent: 'center',
+    overflow: 'hidden',
+    boxShadow: 'none',
+    background: 'none',
   },
   media: {
     height: 500,
   },
   overlay: {
-    position: "absolute",
-    top: "90px",
-    left: "65px",
-    color: "#2A363B",
+    position: 'absolute',
+    top: '90px',
+    left: '65px',
+    color: '#2A363B',
   },
   title: {
     fontSize: 70,
-    fontFamily: "Poppins",
+    fontFamily: 'Poppins',
     fontWeight: 700,
     lineHeight: 1.1,
   },
   subtitle: {
-    fontFamily: "Poppins",
-    marginLeft: "15px",
+    fontFamily: 'Poppins',
+    marginLeft: '15px',
     fontSize: 24,
     fontWeight: 500,
     lineHeight: 1,
   },
   grid: {
-    paddingTop: "30px",
-    paddingLeft: "30px",
-    paddingRight: "30px",
+    paddingTop: '30px',
+    paddingLeft: '30px',
+    paddingRight: '30px',
   },
   card: {
-    background: "none",
-    justifyContent: "center",
-    justify: "center",
-    width: "150%",
-    height: "100px",
+    background: 'none',
+    justifyContent: 'center',
+    justify: 'center',
+    width: '150%',
+    height: '100px',
   },
   wrapper: {
     background:
-      "linear-gradient(135deg, #99B898 0%, #FECEAB 70%, #FF847F 100%)",
-    height: "100rem",
+      'linear-gradient(135deg, #99B898 0%, #FECEAB 70%, #FF847F 100%)',
+    height: '100rem',
   },
   button: {
-    marginTop: "-0.75rem",
-    marginLeft: "-1.2rem",
-    color: "#000000",
+    marginTop: '-0.75rem',
+    marginLeft: '-1.2rem',
+    color: '#000000',
   },
   text: {
-    fontFamily: "Poppins",
+    fontFamily: 'Poppins',
     fontSize: 24,
     fontWeight: 700,
-    marginLeft: "50px",
-    color: "#2A363B",
-    marginBottom: "10px",
+    marginLeft: '50px',
+    color: '#2A363B',
+    marginBottom: '10px',
   },
 });
 
-const fetcher = (url) => fetch(url).then(console.log(url), (res) => res.json());
+// const fetcher = (url) => fetch(url).then(console.log(url), (res) => res.json());
 
 function Profile() {
   const classes = useStyles();
   const router = useRouter();
-  const {slug} = router.query;
-  const { data, error } = useSwr(
-    router.query.id ? `/api/user/${router.query.id}` : null,
-    fetcher
-  )
-  if (error) return <div>Failed to load user</div>
-  if (!data) return <div>Loading...</div>
-  console.log(data);
+  const { slug } = router.query;
+  // const { data, error } = useSwr(
+  //   router.query.id ? `/api/user/${router.query.id}` : null,
+  //   fetcher
+  // );
+
+  // if (error) return <div>Failed to load user</div>;
+  // if (!data) return <div>Loading...</div>;
+  // console.log(data);
+
   return (
     <Paper className={classes.wrapper}>
       <Paper className={classes.root}>
-        <Slide direction="right" in={true} timeout={1000} mountOnEnter>
+        <Slide direction='right' in={true} timeout={1000} mountOnEnter>
           <Card className={classes.root}>
-            <Slide direction="right" in={true} timeout={1000} mountOnEnter>
+            <Slide direction='right' in={true} timeout={1000} mountOnEnter>
               <Box>
                 <Typography className={classes.title}>Your Profile.</Typography>
                 <Grid container className={classes.card} spacing={1}>
-                  <Grid item xs={8} >
+                  <Grid item xs={8}>
                     <Typography className={classes.subtitle}>{slug}</Typography>
                   </Grid>
-                  <Grid item xs={4} >
+                  <Grid item xs={4}>
                     <IconButton
                       onClick={() => navigator.clipboard.writeText(slug)}
                       className={classes.button}
@@ -125,18 +129,15 @@ function Profile() {
           </Card>
         </Slide>
       </Paper>
-      <Slide direction="left" in={true} timeout={1000} mountOnEnter>
+      <Slide direction='left' in={true} timeout={1000} mountOnEnter>
         <Grid container className={classes.grid} spacing={1}>
           <Grid item xs={12}>
             <Typography className={classes.text}>Your NFTs.</Typography>
           </Grid>
-          <NFTcardmini name="NFT 1" id="1" />
-          <NFTcardmini name="NFT 2" id="2" />
-          <NFTcardmini name="NFT 3" id="3" />
-          <NFTcardmini name="NFT 4" id="4" />
+          <NFTlist />
         </Grid>
       </Slide>
-      <Box height="15px" />
+      <Box height='15px' />
     </Paper>
   );
 }
