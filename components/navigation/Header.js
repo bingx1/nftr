468a3d4aa9f51de2d04de51df7cb9e3ea31d0388
Hao Xu <xu.h2@student.unimelb.edu.au>
2021-04-17 12:56:34 +1000

    Fixed NFT header link

--- components/navigation/Header.js
+++ components/navigation/Header.js
@@ -1,15 +1,15 @@
-import { AppBar, IconButton, Toolbar, Typography } from "@material-ui/core";
-import React from "react";
-import AccountCircleIcon from "@material-ui/icons/AccountCircle";
-import { makeStyles } from "@material-ui/styles";
-import Link from "next/link";
-import NavButton from "./NavButton";
-import LoyaltyIcon from "@material-ui/icons/Loyalty";
+import { AppBar, IconButton, Toolbar, Typography } from '@material-ui/core';
+import React from 'react';
+import AccountCircleIcon from '@material-ui/icons/AccountCircle';
+import { makeStyles } from '@material-ui/styles';
+import Link from 'next/link';
+import NavButton from './NavButton';
+import LoyaltyIcon from '@material-ui/icons/Loyalty';
 
 const useStyles = makeStyles(() => ({
   root: {
-    background: "#2A363B",
-    width: "100%",
+    background: '#2A363B',
+    width: '100%',
     zIndex: 100,
     padding: 0,
   },
@@ -19,46 +19,46 @@ const useStyles = makeStyles(() => ({
   },
   logoIcon: {
     flexGrow: 1,
-    paddingLeft: "20px",
+    paddingLeft: '20px',
   },
   accountIcon: {
-    paddingRight: "20px",
+    paddingRight: '20px',
   },
   logoText: {
-    variant: "h1",
-    color: "#f5eacf",
-    fontFamily: "Poppins",
+    variant: 'h1',
+    color: '#f5eacf',
+    fontFamily: 'Poppins',
     fontSize: 18,
     fontWeight: 700,
-    textTransform: "none",
-    boxShadow: "none",
+    textTransform: 'none',
+    boxShadow: 'none',
     backgroundColor: 0,
     borderColor: 0,
-    paddingLeft: "10px",
+    paddingLeft: '10px',
   },
 }));
 
 const Header = () => {
   const classes = useStyles();
   return (
-    <AppBar className={classes.root} position="static">
-      <Toolbar className={classes.toolbar} variant="dense">
-        <Link href="/">
+    <AppBar className={classes.root} position='static'>
+      <Toolbar className={classes.toolbar} variant='dense'>
+        <Link href='/'>
           <a className={classes.logoIcon}>
             <IconButton>
-              <LoyaltyIcon style={{ color: "abcda9" }}></LoyaltyIcon>
+              <LoyaltyIcon style={{ color: 'abcda9' }}></LoyaltyIcon>
               <Typography className={classes.logoText}>NFTR</Typography>
             </IconButton>
           </a>
         </Link>
 
-        <NavButton url="/clubs" text="clubs." />
-        <NavButton url="/nft" text="NFTtest." />
-        <NavButton url="/profile" text="profile." />
-        <Link href="/profile">
+        <NavButton url='/clubs' text='clubs.' />
+        <NavButton url='/nfts' text='NFTs.' />
+        <NavButton url='/profile' text='profile.' />
+        <Link href='/profile'>
           <a className={classes.accountIcon}>
             <IconButton>
-              <AccountCircleIcon style={{ color: "abcda9" }} />
+              <AccountCircleIcon style={{ color: 'abcda9' }} />
             </IconButton>
           </a>
         </Link>
