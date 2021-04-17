ef52149fd5dc15074c4a1f57f3528b0b8ec228c1
bing xu <bingxu.au@gmail.com>
2021-04-18 00:17:02 +1000

    code for interacting with the users contract

--- scripts/interact-users.js
+++ scripts/interact-users.js
@@ -4,7 +4,6 @@ const PUBLIC_KEY = process.env.PUBLIC_KEY;
 const PRIVATE_KEY = process.env.PRIVATE_KEY;
 
 const { createAlchemyWeb3 } = require("@alch/alchemy-web3");
-const { SystemUpdate } = require('@material-ui/icons');
 const web3 = createAlchemyWeb3(API_URL);
 
 const contract = require("../artifacts/contracts/users.sol/users.json"); // for Hardhat
@@ -13,47 +12,32 @@ const usersContract = new web3.eth.Contract(contract.abi, contractAddress);
 
 // Takes in a student ID and fetches the address of the students wallet. 
 async function addStudent(studentid) {
-    return new Promise(async (resolve, reject) => {
-      const nonce = await web3.eth.getTransactionCount(PUBLIC_KEY, "latest"); //get latest nonce
-      const gasEstimate = await usersContract.methods.recordExistance(studentid).estimateGas();
-      //the transaction
-      const tx = {
-        from: PUBLIC_KEY,
-        to: contractAddress,
-        nonce: nonce,
-        gasPrice: gasEstimate,
-        gasLimit: 1000000,
-        data: usersContract.methods.recordExistance(studentid).encodeABI(),
-      };
+    const nonce = await web3.eth.getTransactionCount(PUBLIC_KEY, 'latest'); // get latest nonce
+    const gasEstimate = await usersContract.methods.recordExistance(studentid).estimateGas(); // estimate gas
 
-    //   Sign the transaction
-      const signPromise = web3.eth.accounts.signTransaction(tx, PRIVATE_KEY);
-  
-      signPromise
-        .then((signedTx) => {
-          web3.eth.sendSignedTransaction(signedTx.rawTransaction, function (err, hash) {
-            if (!err) {
-              console.log(
-                "The hash of your transaction is: ",
-                hash,
-                "\nCheck Alchemy's Mempool to view the status of your transaction!"
-              );
-              resolve(hash);
-            } else {
-              console.log(
-                "Something went wrong when submitting your transaction:",
-                err
-              );
-              reject();
-            }
-          });
-        })
-        .catch((err) => {
-          console.log(" Promise failed:", err);
-          reject();
-        });
+    // Create the transaction
+    const tx = {
+      'from': PUBLIC_KEY,
+      'to': contractAddress,
+      'nonce': nonce,
+      'gas': gasEstimate, 
+      'data': usersContract.methods.recordExistance(studentid).encodeABI()
+    };
+
+    // Sign the transaction
+    const signPromise = web3.eth.accounts.signTransaction(tx, PRIVATE_KEY);
+    signPromise.then((signedTx) => {
+      web3.eth.sendSignedTransaction(signedTx.rawTransaction, function(err, hash) {
+        if (!err) {
+          console.log("The hash of your transaction is: ", hash, "\n Check Alchemy's Mempool to view the status of your transaction!");
+        } else {
+          console.log("Something went wrong when submitting your transaction:", err)
+        }
+      });
+    }).catch((err) => {
+      console.log("Promise failed:", err);
     });
-  }
+}
 
 
 
@@ -82,7 +66,17 @@ async function addStudent(studentid) {
 
 async function main() {
     // const res = await usersContract.methods.getAddressFromID(833684).call();
+    // await usersContract.methods.recordExistance(833684).send();
+    // const result = await usersContract.methods.userExists(833684).call();
+    // console.log(result);
     await addStudent(883684);
 }
 
+
 main();
+
+
+// usersContract.methods.recordExistance(833684).send({from: '0x2032267227cd086ceE851C07CCd2A3AdcB77DfBF'})
+// .then(function(receipt){
+//    console.log(receipt); 
+// });
