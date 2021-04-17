02a8b584935b250e4027e64e5ac59468c7742756
jonoe <jono.elkhoury@gmail.com>
2021-04-17 10:49:12 +1000

    your profile page added

---
+++ src/components/pages/profile/Profile.js
@@ -0,0 +1,100 @@
+import React from 'react';
+import {makeStyles} from '@material-ui/core/styles';
+import Grid from '@material-ui/core/Grid';
+import Card from '@material-ui/core/Card';
+import Typography from '@material-ui/core/Typography';
+import {CardActionArea, CardContent, Paper, Slide} from '@material-ui/core';
+import NFTcard from '../NFT/NFTcard'
+
+const useStyles = makeStyles({
+  root: {
+    borderRadius: 0,
+    position: 'relative',
+    width: '100%',
+    height: '20rem',
+    zIndex: 1,
+    display: 'flex',
+    flexWrap: 'wrap',
+    justifyContent: 'space-around',
+    overflow: 'hidden',
+    boxShadow: 'none',
+  },
+  media: {
+    height: 500,
+  },
+  overlay: {
+    position: 'absolute',
+    top: '90px',
+    left: '65px',
+    color: '#2A363B',
+  },
+  title: {
+    fontSize: 70,
+    fontFamily: 'Poppins',
+    fontWeight: 700,
+    lineHeight: 1.1,
+  },
+  subtitle: {
+    fontFamily: 'Poppins',
+    fontSize: 20,
+    fontWeight: 500,
+    lineHeight: 1,
+  },
+  grid: {
+    paddingTop: '30px',
+    paddingLeft: '30px',
+    paddingRight: '30px',
+  },
+  card: {
+    backgroundColor: '#99B898',
+  },
+  wrapper: {
+    background:
+      'linear-gradient(135deg, #99B898 0%, #FECEAB 70%, #FF847F 100%)',
+  },
+  text: {
+    fontFamily: 'Poppins',
+    fontSize: 24,
+    fontWeight: 700,
+    color: '#2A363B'
+  },
+});
+
+function Clubpage() {
+  const classes = useStyles();
+
+  return (
+    <Paper className={classes.wrapper}>
+      <Paper className={classes.root}>
+        <Slide direction='right' in={true} timeout={1000} mountOnEnter>
+          <Card className={classes.root}>
+            <Slide direction='right' in={true} timeout={1000} mountOnEnter>
+              <Typography className={classes.overlay}>
+                <Typography className={classes.title}>
+                  Your Profile.
+              <br />
+                </Typography>
+              </Typography>
+            </Slide>
+          </Card>
+        </Slide>
+      </Paper>
+      <Grid container className={classes.grid} spacing={4}>
+        <Grid item xs={12}>
+          <Paper elevation={0}>
+            <Typography className={classes.text}>
+              Your NFTs.
+          </Typography>
+          </Paper>
+        </Grid>
+        <NFTcard name='NFT 1' id='1' />
+        <NFTcard name='NFT 2' id='2' />
+        <NFTcard name='NFT 3' id='3' />
+        <NFTcard name='NFT 4' id='4' />
+      </Grid>
+    </Paper>
+
+  );
+}
+
+export default Clubpage;
