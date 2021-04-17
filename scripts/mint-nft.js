ce56c52cd7a4c57e5619564fac592327524220be
bing xu <bingxu.au@gmail.com>
2021-04-17 11:04:01 +1000

    removed react/react-route and replaced with next scripts in package.json

--- scripts/mint-nft.js
+++ scripts/mint-nft.js
@@ -1,13 +1,15 @@
 require("dotenv").config();
+// Imports and environment variables
 const API_URL = process.env.API_URL;
 const PUBLIC_KEY = process.env.PUBLIC_KEY;
 const PRIVATE_KEY = process.env.PRIVATE_KEY;
-
 const { createAlchemyWeb3 } = require("@alch/alchemy-web3");
-const web3 = createAlchemyWeb3(API_URL);
-
 const contract = require("../artifacts/contracts/MyNFT.sol/MyNFT.json");
 const contractAddress = "0x2eb1cd1fdcbadc3ccf3f67e1283bafd888b1e7b5";
+
+
+
+const web3 = createAlchemyWeb3(API_URL);
 const nftContract = new web3.eth.Contract(contract.abi, contractAddress);
 
 async function mintNFT(tokenURI) {
