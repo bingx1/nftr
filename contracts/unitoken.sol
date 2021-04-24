437504b9149fd3731d5053169b3b45b2f636b710
bing xu <bingxu.au@gmail.com>
2021-04-24 23:14:41 +1000

    updated the unitoken contract

--- contracts/unitoken.sol
+++ contracts/unitoken.sol
@@ -12,18 +12,34 @@ contract unitoken is ERC721, Ownable {
     using Counters for Counters.Counter;
     Counters.Counter private _tokenIds;
 
-    constructor() public ERC721("uniToken", "NFT") {}
+    mapping (address => bool) public clubs;
 
-    function mintNFT(address recipient, string memory tokenURI)
-        public onlyOwner
-        returns (uint256)
+    event newTokenMinted(address recipient, string tokenURI, address club);
+
+    event logClubRegistered(address club, string name);
+
+    constructor() public ERC721("uniToken", "NFTR") {
+        address msgSender = _msgSender();
+        clubs[msgSender] = true;
+    }
+
+    modifier clubOnly() {
+        require(clubs[msg.sender]);
+        _;
+    }
+
+    function mintNFT(address recipient, string memory tokenURI) public clubOnly returns (uint256)
     {
         _tokenIds.increment();
-
         uint256 newItemId = _tokenIds.current();
         _safeMint(recipient, newItemId);
         _setTokenURI(newItemId, tokenURI);
-
+        emit newTokenMinted(recipient, tokenURI, msg.sender);
         return newItemId;
+    }
+
+    function setClub(address club, string name, bool state) public ownerOnly {
+        clubs[club] = state;
+        emit logClubRegistered(club, name);
     }
 }
